dc3f90b4eb06a3d88c6dc96ed055f3e3b29149bd
Surendiran S <surendiran.ee17@bitsathy.ac.in>
2020-11-04 15:38:56 +0530

    update main.py

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ from os import system
 import linux
 import hadoop
 import aws
+import docker
+import machineLearning
 
+# Banner 
 system("clear;tput setaf 1; tput cup 3 15;tput bold;tput rev");
 print("Python Automation")
 system("tput sgr0;tput bold;tput cup 5 17 ; ");
 print("Main Menu")
 system("tput sgr0");
+
+
+#main menu
 print('''
 	1.  Basic Linux command Automation
 	2.  Hadoop Automation
-	3.  AWS-CLI Automation''')
-system("tput bold")
-print("\n\n\nEnter your choice [1-3] : ",end='')
+	3.  AWS-CLI Automation
+	4.  Docker Automation
+	5.  Machine Learning''')
 
+system("tput bold")
+print("\n\n\nEnter your choice [1-5] : ",end='')
 
 choice = int(input())
 if choice == 1:
@@ -26,6 +34,4 @@ elif choice == 3:
 elif choice == 4:
 	docker.main()
 elif choice == 5:
-	MachineLearning.main()
-
-
+	machineLearning.main()
